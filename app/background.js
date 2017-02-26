d72775f1436e7a070cdcf069940e30ef86e47afc
Andrew Wong <wongandrew97@gmail.com>
2017-02-26 17:08:18 -0500

    Fixed issue when new tab opened infinite tabs

--- app/background.js
+++ app/background.js
@@ -2,9 +2,9 @@ var newTab = function() {
 	chrome.tabs.create({url: chrome.extension.getURL("base.html")});
 };
 
-
 chrome.browserAction.onClicked.addListener(newTab);
 
-
-
+chrome.tabs.onCreated.addListener(function(tab) {
+	chrome.tabs.update(tab.tabId, {url: chrome.extension.getURL("base.html")});
+});
 
